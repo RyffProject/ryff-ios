e9601ecacac7f01ef5e19722f8632fc0355dd1df
Chris Laganiere <ChrisLaganiere@gmail.com>
2014-10-03 14:58:22 -0700

    Create bridging header

---
+++ Ryff/UI/Profile/RiffEdit/RiffCreate/RYRiffEngine.swift
@@ -0,0 +1,13 @@
+//
+//  RYRiffEngine.swift
+//  Ryff
+//
+//  Created by Christopher Laganiere on 10/3/14.
+//  Copyright (c) 2014 Chris Laganiere. All rights reserved.
+//
+
+import UIKit
+
+class RYRiffEngine: NSObject {
+   
+}
